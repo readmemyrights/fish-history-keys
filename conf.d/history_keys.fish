7f8a78b2f327e6f59aa2dc70803d566bf8919369
Sekulum Forka <dictatorofturkmenistan@protonmail.com>
2022-11-19 13:34:02 +0100

    conf.d/history_keys.fish: bound __history_keys_whole

---
+++ conf.d/history_keys.fish
@@ -0,0 +1,7 @@
+# helper function to add this function to both insert and default modes
+function bind.
+    bind $argv
+    bind -M insert $argv
+end
+bind. \e1 __history_keys_whole
+bind. \e! __history_keys_whole
