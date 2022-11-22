481ad2ce8469bc3e49a0547481112f2422bd905c
Sekulum Forka <dictatorofturkmenistan@protonmail.com>
2022-11-22 01:48:59 +0100

    functions/__history_keys_whole.fish: using $history[1] to access last history item instead of head -1

--- functions/__history_keys_whole.fish
+++ functions/__history_keys_whole.fish
@@ -1,4 +1,4 @@
 function __history_keys_whole --description 'insert previous history item'
-    commandline --insert (history |head -1)
+    commandline --insert $history[1]
     commandline -f repaint
 end
